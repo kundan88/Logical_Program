c4854ac5a527b3dcf8dacb40efcacfd95cfa5810
Kundan <kundan kamble>
2022-05-15 23:15:36 -0700

    [KUNDAN] added prime number program

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        int num1;
+        
+        public void readData()
+        {
+            Console.WriteLine("Accept number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void PrimeNo()
+        {
+            if (num1 == 0 || num1 == 1)
+            {
+                Console.WriteLine(num1 + " is not prime number");
+                Console.ReadLine();
+            }
+            else
+            {
+                for (int a = 2; a <= num1 / 2; a++)
+                {
+                    if (num1 % a == 0)
+                    {
+                        Console.WriteLine(num1 + " is not prime number");
+                        return;
+                    }
+
+                }
+                Console.WriteLine(num1 + " is a prime number");
+                Console.ReadLine();
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.readData();
+            p.PrimeNo();
+
+
+        }
+    }
+}
