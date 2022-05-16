0b3f469eded9a1def506a3da0a5478f277bbdc42
Kundan <kundan kamble>
2022-05-15 23:20:42 -0700

    [KUNDAN] added stopwatch program

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    public void StopWatch()
+    {
+        
+        Stopwatch stopwatch = new Stopwatch();
+
+        
+        stopwatch.Start();
+
+        
+        for (int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(1);
+        }
+
+       
+        stopwatch.Stop();
+
+        
+        Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+    }
+    public static void Main(string[] args)
+    {
+        Program p=new Program();
+        p.StopWatch();
+
+    }
+}
